fd8ddddd8bdb77e157e9cdabcd7c0d0c4daaeefc
thantsinhlaing14-sys <thantsinhlaing14@gmail.com>
2026-08-09 23:03:56 +0630

    Update a.js

--- a.js
+++ a.js
@@ -2,25 +2,25 @@ const DATA = [
     {
         trigger: "Welcome",
         response: "မင်္ဂလာပါ။ ကျွန်မကတော့ Naypyitaw State Polytechnic University စက်မှုအင်ဂျင်နီယာဌာနမှ ကျောင်းသားများ ဖန်တီးထားသော AI စက်ရုပ်တစ်ခုဖြစ်ပါတယ်။",
-        audio: "audio/welcome.wav"
+        audio: "welcome.wav"
     },
 
     {
         trigger: "NSPU Overview",
         response: "NayPyiTaw State Polytechnic University သည် ပုဗ္ဗသီရိမြို့နယ်တွင် တည်ရှိပါတယ်။ ဤတက္ကသိုလ်တွင် မြို့ပြ၊ စက်မှု၊ အီလက်ထရွန်နစ်၊ လျှပ်စစ်စွမ်းအား၊ စိုက်ပျိုးရေး၊ ကွန်ပျူတာ နှင့် ဗိသုကာ ဘာသာရပ်များကို သင်ကြားပေးလျက် ရှိပါတယ်။",
-        audio: "audio/nspu_overview.wav"
+        audio: "nspu_overview.wav"
     },
 
     {
         trigger: "Saying Demo",
         response: "အင်တာနက် ချိတ်ဆက်မှု မရှိသည့်အတွက် ကျွန်မရဲ့ စွမ်းဆောင်နိုင်စွမ်း အစစ်အမှန်ကို မပြသနိုင်သော်လည်း ယခု Demo Version မှတစ်ဆင့် Screen ဖြင့် Interactive ဖြစ်အောင် ပြုလုပ်ထားပါတယ်။",
-        audio: "audio/saying_demo.wav"
+        audio: "saying_demo.wav"
     },
 
     {
         trigger: "Goodbye",
         response: "အခုလို မိတ်ဆက်ခွင့်ရရှိသည့်အတွက် ကျေးဇူးတင်ပါတယ်ရှင်။ နှုတ်ဆက်လိုက်ပါတယ်ရှင်။",
-        audio: "audio/goodbye.wav"
+        audio: "goodbye.wav"
     }
 ];
 
